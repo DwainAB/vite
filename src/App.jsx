c2791353a9eedd186366a7d0c8b5cff4c618ca61
Dwain <dwain93290@hotmail.fr>
2025-03-04 11:48:53 +0100

    update app.js

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,39 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function App() {
+
+  const dishes =[
+    {
+      name:"Tacos à l’unité",
+      img:"https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
+      price: 3,
+      isNew: true
+    },
+    {
+      name:"Enchiladas",
+      img:"https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
+      price: 12,
+      isNew: false
+    },
+    {
+      name:"Mole poblano",
+      img:"https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
+      price: 15,
+      isNew: false
+    }
+
+  ]
+
   return (
     <>
       <Header/>
         <Container className="py-5">
           <Row>
-            <Col md={4}>
-              <Dish name="Tacos à l'unité" img="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg" price={3}  isNew={true} />
-            </Col>
-            <Col md={4}>
-              <Dish name="Enchiladas" img="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg" price={12} isNew={false}/>
-            </Col>
-            <Col md={4}>
-              <Dish name="Mole poblano" img="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg" price={15} isNew={false}/>
-            </Col>
+            {dishes.map((item, index)=>(
+              <Col md={4}>
+                <Dish key={index} name={item.name} img={item.img} price={item.price}  isNew={item.isNew} />
+              </Col>
+            ))}
           </Row>
         </Container>
       <Footer/>
